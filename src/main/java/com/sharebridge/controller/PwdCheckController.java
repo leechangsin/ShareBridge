91eaabca527ee0371a925ae372e9936d4c119e93
yichangshin <yichangsin@gmail.com>
2023-03-15 14:19:48 +0900

    #9 회원 정보 수정 페이지 - #8에서 누락된 사항 커밋   -> 비밀번호 확인 컨트롤러에서 불필요한 import 제거

--- src/main/java/com/sharebridge/controller/PwdCheckController.java
+++ src/main/java/com/sharebridge/controller/PwdCheckController.java
@@ -5,8 +5,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
