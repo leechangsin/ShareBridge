316f7aa18ce1368073af38eabc1e87f0446e82a2
yichangshin <yichangsin@gmail.com>
2023-03-15 14:15:49 +0900

    #8 비밀번호 확인 페이지 - 비밀번호 확인 페이지로 이동하기 위해 마이페이지 내 [ 회원정보 수정 ], [ 회원 탈퇴 ] 버튼에 링크 추가

--- src/main/java/com/sharebridge/controller/MypageController.java
+++ src/main/java/com/sharebridge/controller/MypageController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -80,5 +81,19 @@ public class MypageController {
 		model.addAttribute("requestReceiveList", requestReceiveList);
 		
 		return "mypage_main";
+	}
+	
+	@GetMapping(value="/mypage/pwd_check/update.do")
+	public String goFromPwdCheckViewToUpdateView(HttpSession session) {
+		session.setAttribute("redirectURL", "/mypage/update.do");
+		
+		return "redirect:/mypage/pwd_check.do";
+	}
+	
+	@GetMapping(value="/mypage/pwd_check/delete.do")
+	public String goFromPwdCheckViewToDeleteView(HttpSession session) {
+		session.setAttribute("redirectURL", "/mypage/delete.do");
+		
+		return "redirect:/mypage/pwd_check.do";
 	}
 }
