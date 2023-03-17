b83ec8b06285a777507656e577fc721843bd0632
yichangshin <yichangsin@gmail.com>
2023-03-17 14:08:37 +0900

    #40 창신이 개발한 것 중 로그인 필수 기능 적용 - 마이페이지로 접근할 때 로그인하지 않은 상태라면 로그인 페이지로 이동하도록 변경 - 로그인 페이지에서는 로그인을 하지 않은 상태로 자동으로 넘어왔을 때 로그인이 필요한 기능이라고 안내메세지 출력하도록 추가 - 로그인 페이지 내 불필요한 HTML 코드 제거 - 로그인 페이지 내 CSS를 내부 CSS 방식에서 외부 CSS 방식으로 변경

--- src/main/webapp/js/public/common.js
+++ src/main/webapp/js/public/common.js
@@ -15,3 +15,26 @@ function goTo(url) {
 	
 	location.href = url;
 }
+
+function getParameter(name) {
+	let parameters = location.search;
+	
+	if(parameters == '') {
+		return undefined;
+	}
+	
+	parameters = parameters.substring(1);
+	parameters = parameters.split("&");
+	
+	for(let i=0; i<parameters.length; i++) {
+		let parameter = parameters[i].split("=");
+		let paramName = parameter[0];
+		let paramValue = parameter[1];
+		
+		if(paramName == name) {
+			return paramValue;
+		}
+	}
+	
+	return undefined;
+}
