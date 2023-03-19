f2d7c908e6507c81466bd0fcc6f69e1c68aee3a3
yichangshin <yichangsin@gmail.com>
2023-03-19 20:14:40 +0900

    프로젝트에 알 수 없는 문제가 생겨 문제가 생기는 파일을 다시 빌드하도록 소소한 수정

--- src/main/java/com/sharebridge/dto/WishDto.java
+++ src/main/java/com/sharebridge/dto/WishDto.java
@@ -3,14 +3,12 @@ package com.sharebridge.dto;
 import java.time.LocalDateTime;
 
 public class WishDto {
-
 	private int product_id;
 	private int member_id;
 	private LocalDateTime rdate;
 
 	public WishDto() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public WishDto(int product_id, int member_id, LocalDateTime rdate) {
